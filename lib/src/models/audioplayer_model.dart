ea8a5def5ce7d25a46eb832e8121cc5969f9c63f
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-23 14:03:48 -0300

    FM189 - get the duration of the song

--- lib/src/models/audioplayer_model.dart
+++ lib/src/models/audioplayer_model.dart
@@ -4,11 +4,55 @@ class AudioPlayerModel with ChangeNotifier{
 
   bool _playing = false;
 
+  Duration _songDuration = new Duration(milliseconds: 0);
+  Duration _current      = new Duration(milliseconds: 0);
+
+  String get songTotalDuration => this.printDuration( this._songDuration );
+  String get currentSecond     => this.printDuration( this._current );
+
+  double get percentage => (this._songDuration.inSeconds > 0) 
+                            ? this._current.inSeconds / this._songDuration.inSeconds
+                            : 0 ;
+
+
+
   late AnimationController _controller;
   set controller(AnimationController value){
     this._controller = value;
   }
 
   AnimationController get controller => this._controller;
+
+  bool get playing => this._playing;
+  set playing(bool value){
+    this._playing = value;
+    notifyListeners();
+  }
+
+  Duration get songDuration => this._songDuration;
+  set songDuration(Duration value){
+    this._songDuration = value;
+    notifyListeners();
+  }
+  Duration get current => this.current;
+  set current(Duration value){
+    this._current = value;
+    notifyListeners();
+  }
+
+  String printDuration(Duration duration){
+
+    String twoDigits(int n){
+      if(n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitMinutes = twoDigits( duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits( duration.inSeconds.remainder(60));
+
+    return "$twoDigitMinutes:$twoDigitSeconds";
+
+  }
+
 
 }
