d99b448f3587652ca1ca04aac766c2b93981b93e
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-23 11:47:50 -0300

    FM178 - Image Disc

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -11,8 +11,80 @@ class MusicPlayerPage extends StatelessWidget {
         children: <Widget>[
           
           CustomAppBar(),
+
+          ImageDiscDuration(),
         ],
       )
    );
+  }
+}
+
+class ImageDiscDuration extends StatelessWidget { 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+ 
+          ImageDisc(),
+
+          //TODO: Barra progreso
+
+
+
+        ],
+      ),
+    );
+  }
+}
+
+class ImageDisc extends StatelessWidget { 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image( image: AssetImage('assets/Cover.jpg')),
+
+            Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                color: Colors.black38, 
+                borderRadius: BorderRadius.circular(100)
+              ),
+            ),
+
+            Container(
+              width: 18,
+              height: 18,
+              decoration: BoxDecoration(
+                color: Color(0xff1C1C25), 
+                borderRadius: BorderRadius.circular(100)
+              ),
+            ),
+
+
+          ],
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xff484750),
+            Color(0xff11C24),
+          ]
+        )
+      ),
+    );
   }
 }
